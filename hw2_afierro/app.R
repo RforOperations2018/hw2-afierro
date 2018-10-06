bb86cc6f17152f99417e433167ff2306a9d07bea
AllysonAndreaFierro <42786340+AllysonAndreaFierro@users.noreply.github.com>
2018-10-05 21:52:32 -0400

    Trying to add my Race Input, got an error

--- hw2_afierro/app.R
+++ hw2_afierro/app.R
@@ -93,7 +93,7 @@ server <- function(input, output, session = session) {
   # Filtered arrests data
     PInput <- reactive({
       
-      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22e03a89dd-134a-4ee8-a2bd-62c40aeebc6f%22%20WHERE%20%22CREATED_ON%22%20%3E=%20%27", input$ageSelect[1], "%27%20AND%20%22CREATED_ON%22%20%3C=%20%27", input$ageSelect[2], "%27%20AND%20%22Neighborhood%22%20=%20%27", input$NeighborhoodSelect, "%27")
+      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22e03a89dd-134a-4ee8-a2bd-62c40aeebc6f%22%20WHERE%20%22CREATED_ON%22%20%3E=%20%27", input$ageSelect[1], "%27%20AND%20%22CREATED_ON%22%20%3C=%20%27", input$ageSelect[2], "%27%20AND%20%22Neighborhood%22%20=%20%27", input$NeighborhoodSelect, "%27%20AND%20%22Race%22%20=%20%27", input$RaceSelect,"%27")
       
       arrests <- ckanSQL(url) %>%
 # Age Slider Filter
@@ -101,7 +101,7 @@ server <- function(input, output, session = session) {
       
 # Neighborhood Filter
     if (length(input$NeighborhoodSelect) > 0 ) {
-      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22e03a89dd-134a-4ee8-a2bd-62c40aeebc6f%22%20WHERE%20%22CREATED_ON%22%20%3E=%20%27", input$ageSelect[1], "%27%20AND%20%22CREATED_ON%22%20%3C=%20%27", input$ageSelect[2], "%27%20AND%20%22Neighborhood%22%20=%20%27", input$NeighborhoodSelect, "%27")
+      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%22e03a89dd-134a-4ee8-a2bd-62c40aeebc6f%22%20WHERE%20%22CREATED_ON%22%20%3E=%20%27", input$ageSelect[1], "%27%20AND%20%22CREATED_ON%22%20%3C=%20%27", input$ageSelect[2], "%27%20AND%20%22Neighborhood%22%20=%20%27", input$NeighborhoodSelect, "%27%20AND%20%22Race%22%20=%20%27", input$RaceSelect, "%27")
       arrests <- ckanSQL(url)
   }
                               
